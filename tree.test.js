74ad31622e58c5d64496f647fc493302c19207da
Roxanoel <roxane.noel92@gmail.com>
2022-08-06 12:43:35 +0100

    Wrote tests for tree building. Including array preparation (sorting + removing duplicates).

--- tree.test.js
+++ tree.test.js
@@ -6,20 +6,25 @@ beforeEach(() => {
 });
 
 describe('Building a tree', () => {
-    test('prepareArray sorts the array', () => {
-
+    test('prepareArray sorts provided array', () => {
+        expect(tree.prepareArray([1, 7, 4, 23, 8, 9])).toStrictEqual([1, 4, 7, 8, 9, 23]);
     });
 
-    test('prepareArray removes duplicates', () => {
+    test('prepareArray removes duplicates from provided array', () => {
+        expect(tree.prepareArray([1, 7, 4, 23, 8, 9, 4])).toStrictEqual([1, 4, 7, 8, 9, 23]);
+    });
 
+    test('buildTree throws error if what is passed in is not an array', () => {
+        expect(() => { tree.prepareArray(1)}).toThrow('Please pass in an array');
     });
     
     test('buildTree returns level-0 root node', () => {
-
+        expect(tree.buildTree([1, 7, 4, 23, 8, 9, 4]).data).toBe(7);
     });
 
     test('Root node returned by buildTree has correct left and right child nodes', () => {
-
+        expect(tree.buildTree([1, 7, 4, 23, 8, 9, 4]).left.data).toBe(4);
+        expect(tree.buildTree([1, 7, 4, 23, 8, 9, 4]).right.data).toBe(9);
     });
 });
 describe.skip('Inserting and deleting nodes', () => {
