21b009e8fcb5d042a7542c5643709e990dda9dd9
Roxanoel <roxane.noel92@gmail.com>
2022-08-06 12:28:36 +0100

    Wrote skeleton for tests. No test contents yet.

--- tree.test.js
+++ tree.test.js
@@ -1,1 +1,39 @@
 const Tree = require('./tree');
+
+let tree;
+beforeEach(() => {
+
+});
+
+describe('Building a tree', () => {
+    test('prepareArray sorts the array', () => {
+
+    });
+
+    test('prepareArray removes duplicates', () => {
+
+    });
+    
+    test('buildTree returns level-0 root node', () => {
+
+    });
+
+    test('Root node returned by buildTree has correct left and right child nodes', () => {
+
+    });
+});
+describe.skip('Inserting and deleting nodes', () => {
+
+});
+describe.skip('levelOrder traversal', () => {
+    // Write a mock function to use as callback parameter?
+});
+describe.skip('Inorder, preorder, and postoder traversal', () => {
+
+});
+describe.skip('Height & depth', () => {
+
+});
+describe.skip('Balancing', () => {
+
+});
